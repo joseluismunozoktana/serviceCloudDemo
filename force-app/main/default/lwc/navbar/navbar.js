46b1ae997ef16a6a1e86e765d491b53a539a7bda
joseluismunozoktana <jose.munoz@oktana.com>
2022-08-18 04:16:44 -0400

    avance

---
+++ force-app/main/default/lwc/navbar/navbar.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import logo from '@salesforce/resourceUrl/adtlogozip';
+
+export default class Navbar extends LightningElement {
+    adtlogo = logo + '/adt-new-logo.svg';
+}
---
+++ force-app/main/default/lwc/warranty/warranty.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import flooddevice from '@salesforce/resourceUrl/flooddevice';
+
+export default class Warranty extends LightningElement {
+    deviceimage = flooddevice + '/flooddeviceADT.png';
+}
