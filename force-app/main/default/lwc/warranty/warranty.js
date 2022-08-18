d2cadd2cf0dadad74547640ee195f74aef414ff1
joseluismunozoktana <jose.munoz@oktana.com>
2022-08-18 11:00:27 -0400

    secondSlideFinished

--- force-app/main/default/lwc/warranty/warranty.js
+++ force-app/main/default/lwc/warranty/warranty.js
@@ -3,4 +3,29 @@ import flooddevice from '@salesforce/resourceUrl/flooddevice';
 
 export default class Warranty extends LightningElement {
     deviceimage = flooddevice + '/flooddeviceADT.png';
+    value = '2years';
+    secondvalue = 'full';
+
+    get options() {
+        return [
+            { label: '1 year', value: '1year' },
+            { label: '2 years', value: '2years' },
+            { label: '3 years', value: '3years' },
+        ];
+    }
+
+    get options2() {
+        return [
+            { label: 'Full', value: 'full' },
+            { label: 'Partial', value: 'partial' }
+        ];
+    }
+
+    handleChange(event) {
+        this.value = event.detail.value;
+    }
+
+    handleChange2(event) {
+        this.secondvalue = event.detail.value;
+    }
 }
