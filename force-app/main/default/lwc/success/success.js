ecf990f57c87abef475721e807cc76e9fb3eab3b
joseluismunozoktana <jose.munoz@oktana.com>
2022-08-25 12:20:22 -0500

    finalchanges

--- force-app/main/default/lwc/success/success.js
+++ force-app/main/default/lwc/success/success.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
+import flooddetector from '@salesforce/resourceUrl/flooddetector';
+import greencheck from '@salesforce/resourceUrl/greencheck';
 
-export default class Success extends LightningElement {}
+
+export default class Success extends LightningElement {
+    justdevice = flooddetector + '/flood-detector.png';
+    check = greencheck + '/greencheck.png';
+}
