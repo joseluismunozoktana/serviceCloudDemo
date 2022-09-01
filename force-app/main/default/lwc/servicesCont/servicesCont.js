54086f015070225e1ab409af09d62730ca7790b3
joseluismunozoktana <jose.munoz@oktana.com>
2022-09-01 13:03:01 -0500

    with Learn more in devinstcont

--- force-app/main/default/lwc/servicesCont/servicesCont.js
+++ force-app/main/default/lwc/servicesCont/servicesCont.js
@@ -1,10 +1,10 @@
 import { LightningElement } from 'lwc';
 import imgfolder from '@salesforce/resourceUrl/adt';
 export default class ServicesCont extends LightningElement {
-    img1 = imgfolder + '/cloudimages/img1.jpg';
-    img2 = imgfolder + '/cloudimages/img2.jpg';
-    img3 = imgfolder + '/cloudimages/img3.jpg';
-    img4 = imgfolder + '/cloudimages/img4.jpg';
-    img5 = imgfolder + '/cloudimages/img5.jpg';
-    img6 = imgfolder + '/cloudimages/img6.jpg';
+    img1 = imgfolder + '/cloudimages/img1.png';
+    img2 = imgfolder + '/cloudimages/img2.png';
+    img3 = imgfolder + '/cloudimages/img3.png';
+    img4 = imgfolder + '/cloudimages/img4.png';
+    img5 = imgfolder + '/cloudimages/img5.png';
+    img6 = imgfolder + '/cloudimages/img6.png';
 }
