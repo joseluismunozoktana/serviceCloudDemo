e17c56623d4f3ff325d29b6e2d5d614df32dc05f
joseluismunozoktana <jose.munoz@oktana.com>
2022-09-12 11:00:41 -0500

    finalChanges

--- force-app/main/default/lwc/devInstCont/devInstCont.js
+++ force-app/main/default/lwc/devInstCont/devInstCont.js
@@ -1,10 +1,7 @@
 import { LightningElement } from 'lwc';
 import imgfolder from '@salesforce/resourceUrl/adt';
 export default class DevInstCont extends LightningElement {
-    img1 = imgfolder + '/cloudimages/img1.png';
-    img2 = imgfolder + '/cloudimages/img2.png';
-    img3 = imgfolder + '/cloudimages/img3.png';
-    img4 = imgfolder + '/cloudimages/img4.png';
-    img5 = imgfolder + '/cloudimages/img5.png';
-    img6 = imgfolder + '/cloudimages/img6.png';
+    img1 = imgfolder + '/mainscreen/img1.png';
+    img2 = imgfolder + '/mainscreen/img2.png';
+    img3 = imgfolder + '/mainscreen/img3.png';
 }
--- force-app/main/default/lwc/servicesCont/servicesCont.js
+++ force-app/main/default/lwc/servicesCont/servicesCont.js
@@ -3,8 +3,8 @@ import imgfolder from '@salesforce/resourceUrl/adt';
 export default class ServicesCont extends LightningElement {
     img1 = imgfolder + '/cloudimages/img1.png';
     img2 = imgfolder + '/cloudimages/img2.png';
-    img3 = imgfolder + '/cloudimages/img3.png';
     img4 = imgfolder + '/cloudimages/img4.png';
     img5 = imgfolder + '/cloudimages/img5.png';
     img6 = imgfolder + '/cloudimages/img6.png';
+    img3 = imgfolder + '/cloudimages/img3.png';
 }
